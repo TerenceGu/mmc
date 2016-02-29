4067a4283e9db7e4333691d5b351e05ff3ab28b2
renaesop <njurjs@gmail.com>
2016-02-29 09:27:04 +0800

    将pss.json改为pss.js 其uri配置信息从config中读取

--- config.js
+++ config.js
@@ -4,8 +4,14 @@
 'use strict';
 
 const path = require('path');
+
 module.exports = {
   port: 4000,
   mapJson: path.join(__dirname, 'front', 'dist', 'map.json'),
-  hostname: '0.0.0.0'
+  hostname: '0.0.0.0',
+  uri: {
+    baseUri: process.env.address || 'localhost:4000',
+    protocol: '//',
+    suffix: '',
+  },
 };
