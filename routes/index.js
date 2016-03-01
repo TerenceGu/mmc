8bc523eeceabb8f8fc1de37023e74b44ff3a44f7
renaesop <njurjs@gmail.com>
2016-03-01 09:38:37 +0800

    将引入controller单独提成模块

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 /**
  * Created by Administrator on 2016/2/23.
  */
-const exportController = require('../lib/util').exportController;
+const exportController = require('lt-util').exportController;
 
-module.exports = exportController(__dirname);
+const router = exportController(__dirname);
+
+router.get('/i', function*() {
+   yield this.render('hello',{
+       name: 'i',
+       lang: 'zh'
+   });
+});
+
+module.exports = router;
