8f44561c6a67232331337254c12db2eec5397767
renaesop <njurjs@gmail.com>
2016-03-01 20:48:23 +0800

    增加minifier模块 在production环境变量时会生效

--- app.js
+++ app.js
@@ -12,7 +12,17 @@ const app = koa();
 
 app.use(logger());
 
-app.use(require('koa-static')(path.join(__dirname, 'front', 'dist')));
+if(process.env.NODE_ENV === 'production'){
+  app.use(require('koa-html-minifier')({
+    collapseWhitespace: true,
+    removeComments: true,
+    removeCommentsFromCDATA: true,
+    minifyJS: true
+  }));
+}
+else{
+  app.use(require('koa-static')(path.join(__dirname, 'front', 'dist')));
+}
 
 app.use(hbs.middleware({
   viewPath: path.join(__dirname, 'views'),
