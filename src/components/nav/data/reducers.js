903204e2fc210542a90edaa19562331e46c7a93f
Raven <x@x.com>
2016-04-14 13:44:22 +0800

    #3 A.商品类别输出改为循环

--- src/components/nav/data/reducers.js
+++ src/components/nav/data/reducers.js
@@ -2,7 +2,109 @@
  * Created by fed on 16/4/8.
  */
 const defaultState = {
-  lowerFix:false
+  lowerFix: false,
+  categories: {
+    'JUST IN': {
+      link: '#',
+      children: {
+        Dresses: '#',
+        Top: '#',
+        'Hoodies & Sweatshirts': '#',
+        Sweaters: '#',
+        Outerwear: '#',
+        Kimono: '#',
+        'Jumpsuits & Rompers': '#',
+        'Co-ordinates': '#',
+        Shoes: '#',
+        Accessories: '#',
+        Bottoms: '#',
+        Swimwear: '#'
+      }
+    },
+    CLOTHING: {
+      link: '#',
+      children: {
+        Dresses: '#',
+        Top: '#',
+        'Hoodies & Sweatshirts': '#',
+        Sweaters: '#',
+        Outerwear: '#',
+        Kimono: '#',
+        'Jumpsuits & Rompers': '#',
+        'Co-ordinates': '#',
+        Shoes: '#',
+        Accessories: '#',
+        Bottoms: '#',
+        Swimwear: '#'
+      }
+    },
+    DRESSES: {
+      link: '#',
+      children: {
+        'Boho Dresses': '#',
+        'Casual Dresses': '#',
+        'Floral Dresses': '#',
+        'Maxl Dresses': '#',
+        'Party Dresses': '#',
+        'Bodycon Dresses': '#',
+        'Lace Dresses': '#',
+        'Backless Dresses': '#',
+        'MIDI Dresses': '#',
+        'CutOut Dresses': '#',
+        'Skater Dresses': '#'
+      }
+    },
+    SHOES: {
+      link: '#',
+      children: {
+        'High Heels': '#',
+        Sandals: '#',
+        Boots: '#',
+        Wedges: '#',
+        Flats: '#'
+      }
+    },
+    OUTERWEAR: {
+      link: '#',
+      children: {
+        Coats: '#',
+        Jackets: '#',
+        Blazers: '#',
+        Vests: '#'
+      }
+    },
+    ACCESSORIES: {
+      link: '#',
+      children: {
+        Handbags: '#',
+        Necklace: '#',
+        'Earrings & Sweatshirts': '#',
+        Bracelets: '#',
+        Rings: '#',
+        Scaves: '#',
+        Hats: '#',
+        Belts: '#',
+        Sunglasses: '#'
+      }
+    },
+    SALE: {
+      link: '#',
+      children: {
+        Dresses: '#',
+        Top: '#',
+        'Hoodies & Sweatshirts': '#',
+        Sweaters: '#',
+        Outerwear: '#',
+        Kimono: '#',
+        'Jumpsuits & Rompers': '#',
+        'Co-ordinates': '#',
+        Shoes: '#',
+        Accessories: '#',
+        Bottoms: '#',
+        Swimwear: '#'
+      }
+    }
+  }
 };
 
 export default (state = defaultState, action) => {
@@ -10,8 +112,8 @@ export default (state = defaultState, action) => {
     case '<%= appnameUpper %>_INIT':
       return state;
     case 'NAV_SET_LOWER_FIX':
-      return Object.assign({},state,{
-        lowerFix:action.flag
+      return Object.assign({}, state, {
+        lowerFix: action.flag
       });
     default:
       return state;
--- src/components/nav/views/container.jsx
+++ src/components/nav/views/container.jsx
@@ -8,195 +8,90 @@ import SearchBar from './search-bar.jsx';
 import Login from './signIn.jsx';
 import ShoppingBag from './shoppingBag.jsx';
 
-import {setLowerFix} from '../data/actions.js';
+import { setLowerFix } from '../data/actions.js';
 
 import styles from './container.scss';
 
-const Container = ({ dispatch , lowerFix }) => {
+const Container = ({ dispatch, lowerFix, categories }) => {
+  let lower;
+  let upper;
+  window.onload = function () {
+    function scrollListener() {
+      if (lower.getBoundingClientRect().top <= 0
+        && upper.getBoundingClientRect().bottom <= 0) {
+        dispatch(setLowerFix(true));
+      } else if (upper.getBoundingClientRect().bottom > 0) {
+        dispatch(setLowerFix(false));
+      }
+    }
 
-  window.onload=function(){
-      let lower=document.getElementsByClassName(styles.lowerHeader)[0];
-      let upper=document.getElementsByClassName(styles.upperHeader)[0];
-      function scrollListener(event){
-          if(lower.getBoundingClientRect().top<=0
-              &&upper.getBoundingClientRect().bottom<=0){
-              dispatch(setLowerFix(true));
-          }else if(upper.getBoundingClientRect().bottom>0){
-              dispatch(setLowerFix(false));
-          }
-      }
-      function deBounce(fun,timeout){
-          let prev=Date.now();
-          return function(){
-              let now=Date.now();
-              if(now-prev>=timeout){
-                  prev=now;
-                  fun.apply(this,arguments);
-              }
-          }
-      }
-      window.addEventListener("scroll",deBounce(scrollListener,0),false)
+    function deBounce(fun, timeout) {
+      let prev = Date.now();
+      return function (...args) {
+        const now = Date.now();
+        if (Date.now() - prev >= timeout) {
+          prev = now;
+          fun.apply(this, args);
+        }
+      };
+    }
+
+    window.addEventListener('scroll', deBounce(scrollListener, 0), false);
   };
   return (
     <div className={styles.nav}>
-        <div className={styles.upperHeader}>
-            <div className={styles.upperHeaderInner}>
-                <a href="#"><img src="http://statics.makemechic.com/image/logo_v6571b05.png" /></a>
-                <div className={styles.headerRight}>
-                    <div className={styles.logoRight}>
-                        <ShoppingBag goodsNum={0} />
-                        <Login isLogin={false} />
-                    </div>
-                    <div style={{width:"325px"}}>
-                        <SearchBar />
-                    </div>
-                </div>
+      <div className={styles.upperHeader} ref={ node => { upper = upper || node; } }>
+        <div className={styles.upperHeaderInner}>
+          <a href="#"><img src="http://statics.makemechic.com/image/logo_v6571b05.png" /></a>
+          <div className={styles.headerRight}>
+            <div className={styles.logoRight}>
+              <ShoppingBag goodsNum={0} />
+              <Login isLogin={false} />
             </div>
+            <div style={{ width: 325 }}>
+              <SearchBar />
+            </div>
+          </div>
         </div>
-      <div className={lowerFix?classnames(styles.lowerHeader,styles.fix):classnames(styles.lowerHeader)}>
-        <ul>
-          <li>
-              <a href="#">JUST IN</a>
-              <div className="list">
-                  <ul className={styles.justIn}>
-                      <li><a href="#"> Dresses</a></li>
-                      <li><a href="#">Top</a></li>
-                      <li><a href="#">Hoodies & Sweatshirts</a></li>
-                      <li><a href="#">Sweaters</a></li>
-                      <li><a href="#">Outerwear</a></li>
-                      <li><a href="#">Kimono</a></li>
-                      <li><a href="#">Jumpsuits & Rompers</a></li>
-                      <li><a href="#">Co-ordinates</a></li>
-                      <li><a href="#">Shoes</a></li>
-                      <li><a href="#">Accessories</a></li>
-                      <li><a href="#">Bottoms</a></li>
-                      <li><a href="#">Swimwear</a></li>
-                  </ul>
-              </div>
-          </li>
-          <li>
-              <a href="#">
-                  CLOTHING
-              </a>
-                  <div className="list">
-                      <ul className={styles.justIn}>
-                          <li><a href="#"> Dresses</a></li>
-                          <li><a href="#">Top</a></li>
-                          <li><a href="#">Hoodieees & Sweatshirts</a></li>
-                          <li><a href="#">Sweaters</a></li>
-                          <li><a href="#">Outerwear</a></li>
-                          <li><a href="#">Kimono</a></li>
-                          <li><a href="#">Jumpsuits & Rompers</a></li>
-                          <li><a href="#">Co-ordinates</a></li>
-                          <li><a href="#">Shoes</a></li>
-                          <li><a href="#">Accessories</a></li>
-                          <li><a href="#">iuo raet</a></li>
-                          <li><a href="#">Swimwear</a></li>
-                      </ul>
-                  </div>
-          </li>
-          <li>
-              <a href="#">
-                  DRESSES
-              </a>
-              <div className="list">
-                  <ul className={styles.justIn}>
-                      <li><a href="#">Boho Dresses</a></li>
-                      <li><a href="#">Casual Dresses</a></li>
-                      <li><a href="#">Floral Dresses</a></li>
-                      <li><a href="#">Maxl Dresses</a></li>
-                      <li><a href="#">Party Dresses</a></li>
-                      <li><a href="#">Bodycon Dresses</a></li>
-                      <li><a href="#">Lace Dresses</a></li>
-                      <li><a href="#">Backless Dresses</a></li>
-                      <li><a href="#">MIDI Dresses</a></li>
-                      <li><a href="#">CutOut Dresses</a></li>
-                      <li><a href="#">Skater Dresses</a></li>
-                  </ul>
-              </div>
-          </li>
-          <li>
-              <a href="#">
-                  SHOES
-              </a>
-              <div className="list">
-                  <ul className={styles.justIn}>
-                      <li><a href="#">High Heels</a></li>
-                      <li><a href="#">Sandals</a></li>
-                      <li><a href="#">Boots</a></li>
-                      <li><a href="#">Wedges</a></li>
-                      <li><a href="#">Flats</a></li>
-                  </ul>
-              </div>
-          </li>
-          <li>
-              <a href="#">
-                  OUTERWEAR
-              </a>
-              <div className="list">
-                  <ul className={styles.justIn}>
-                      <li><a href="#">Coats</a></li>
-                      <li><a href="#">Jackets</a></li>
-                      <li><a href="#">Blazers</a></li>
-                      <li><a href="#">Vests</a></li>
-                  </ul>
-              </div>
-          </li>
-          <li>
-              <a href="#">
-                  ACCESSORIES
-              </a>
-              <div className="list">
-                  <ul className={styles.justIn}>
-                      <li><a href="#">Handbags</a></li>
-                      <li><a href="#">Necklace</a></li>
-                      <li><a href="#">Earrings & Sweatshirts</a></li>
-                      <li><a href="#">Bracelets</a></li>
-                      <li><a href="#">Rings</a></li>
-                      <li><a href="#">Scaves</a></li>
-                      <li><a href="#">Hats</a></li>
-                      <li><a href="#">Belts</a></li>
-                      <li><a href="#">Sunglasses</a></li>
-                  </ul>
-              </div>
-          </li>
-          <li>
-              <a href="#">
-                  SALE
-              </a>
-              <div className="list">
-                  <ul className={styles.justIn}>
-                      <li><a href="#">Dresses</a></li>
-                      <li><a href="#">Top</a></li>
-                      <li><a href="#">Hoodies & Sweatshirts</a></li>
-                      <li><a href="#">Sweaters</a></li>
-                      <li><a href="#">Outerwear</a></li>
-                      <li><a href="#">Kimono</a></li>
-                      <li><a href="#">Jumpsuits & Rompers</a></li>
-                      <li><a href="#">Co-ordinates</a></li>
-                      <li><a href="#">Shoes</a></li>
-                      <li><a href="#">Accessories</a></li>
-                      <li><a href="#">Bottoms</a></li>
-                      <li><a href="#">Swimwear</a></li>
-                  </ul>
-              </div>
-          </li>
-          {lowerFix? <li className={styles.searchBarLi}><SearchBar /></li>:null}
-          {lowerFix? <li><ShoppingBag goodsNum={0} /></li>:null}
-        </ul>
       </div>
-        <div style={{width:"100%",height:"1500px",clear:"both"}}></div>
+      <div className={lowerFix ? classnames(styles.lowerHeader, styles.fix) :
+      classnames(styles.lowerHeader)} ref={ node => { lower = lower || node; } }
+      >
+      <ul>
+        {
+          Object.keys(categories).map(value => (
+            <li>
+              <a href={categories[value].link}>{value}</a>
+              <div className={ styles.list }>
+                <ul className={styles.justIn}>
+                  {
+                    Object.keys(categories[value].children).map(child => (
+                      <li><a href={categories[value].children[child]}>{child}</a></li>
+                    ))
+                  }
+                </ul>
+              </div>
+            </li>
+          ))
+        }
+        {lowerFix ? <li className={styles.searchBarLi}><SearchBar /></li> : null}
+        {lowerFix ? <li><ShoppingBag goodsNum={0}/></li> : null}
+      </ul>
+      </div>
+      <div style={{ width: '100%', height: '1500px', clear: 'both' }}></div>
     </div>
   );
 };
 
 Container.propTypes = {
-    lowerFix:React.PropTypes.bool.isRequired
+  lowerFix: React.PropTypes.bool.isRequired,
+  categories: React.PropTypes.bool.isRequired
 };
 
 const mapStateToProps = ({ nav }) => (
 {
-    lowerFix: nav.lowerFix
+  lowerFix: nav.lowerFix,
+  categories: nav.categories
 }
 );
 
