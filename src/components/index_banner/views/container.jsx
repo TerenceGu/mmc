54e1b1d821e03acafa5e7984f5e94c090e665c31
renaesop <njurjs@gmail.com>
2016-04-14 16:58:13 +0800

    通过slider容器完成index banner

--- src/components/index_banner/views/container.jsx
+++ src/components/index_banner/views/container.jsx
@@ -1,23 +1,38 @@
 /**
- * Created by fed on 16/4/8.
+ * Created by fed on 16/4/14.
  */
-import React, { PropTypes } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
-import styles from './container.scss';
+import styles from './container.jsx';
+import Slider from '../../../common/containers/slide/index.jsx';
 
+const SlideWrapper = connect()(Slider);
 
-const Container = ({ dispatch }) => {
-  return (
-    <div>
-      
-    </div>
-  );
+const props = {
+  width: 960,
+  height: 555
 };
 
-Container.propTypes = {
-  
+const Wrapped = ({ list }) => (
+  <SlideWrapper {...props}>
+    {
+      list.map(({ link, img }) => (
+        <a href={link} className={styles.link} key={link}>
+          <img src={img} {...props} />
+        </a>
+      ))
+    }
+  </SlideWrapper>
+);
+
+Wrapped.propTypes = {
+  list: React.PropTypes.array
 };
 
+const mapStateToProps = ({ index_banner }) => ({
+  list: index_banner.list
+});
 
-export default connect()(Container);
+
+export default connect(mapStateToProps)(Wrapped);
